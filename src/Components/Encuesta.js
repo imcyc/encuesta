e959b57c1e5b711626db5d803453bce1d8844bba
Axel <axosar@gmail.com>
2020-01-27 17:47:35 -0600

    respuesta formulario

--- src/Components/Encuesta.js
+++ src/Components/Encuesta.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -8,6 +9,7 @@ import Header from "./Header";
 import myData from '../Datos/Datos.json';
 
 const Encuesta = () => {
+  const [formulario, setFormulario] = useState(true);
   const [seccionUno, setSeccionUno] = useState(true);
   const [seccionDos, setSeccionDos] = useState(false);
 
@@ -26,83 +28,111 @@ const Encuesta = () => {
 
   const enviarDatos = (e) => {
     e.preventDefault();
-    console.log('mandar datos');
-    const r1 = e.target.nombre_completo.value;
-    console.log(r1);
-    const ro1 = e.target.customRadioInline1.value;
-    console.log(ro1);
+
+    const user = {
+      r1: e.target.nombre_completo.value,
+      ro1: e.target.customRadioInline1.value
+    };
+
+    axios.post(`http://www.imcyc.com/biblioteca/apiencuesta.php`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth' });
+    setFormulario(!formulario);
   }
 
   return(
     <div>
       <Header />
       <Container className="mb-5">
-      <Form onSubmit={enviarDatos}>
-        <div style={style}>
-          <Row>
-            <Col>
-              <hr/>
-                <h1 className="text-left">1) DATOS DE USUARIO:</h1>
-              <hr/>
-            </Col>
-          </Row>
-          <Row>
-            <Col style={{marginTop: '15px'}}>
-              {datosPersonalesPreguntas.map((pregunta, index) => (
-                <Form.Control 
-                  key={index} 
-                  name={pregunta.valor} 
-                  placeholder={pregunta.nombre} 
-                  type={pregunta.tipo} 
-                  style={{marginBottom: '10px'}}
-                />
-              ))}
-            </Col>
-          </Row>
-          <Button variant="outline-light" className="cambioSeccion" onClick={datosPersonales}>SIGUIENTE</Button>
-        </div>
-        <div style={styleDos}>
-          <Row>
-            <Col>
-              <hr/>
-                <h1 className="text-left font-weight-bold">2) ATENCIÓN AL CLIENTE:</h1>
-              <hr/>
-            </Col>
-          </Row>
-          <Row>
-            <Col style={{marginTop: '15px'}}>
-              {datosImcycPreguntas.map((pregunta, i) => (
-                <div key={i} style={{marginBottom: '40px'}}>
-                  <h3 className="text-left" className="numero">{i + 1}</h3>
-                  <h2 className="text-left preguntame">{pregunta.pregunta}</h2>
-                  <Row>
-                    {pregunta.opciones.map((opcion, index) => (
-                        <Col key={index}>
-                          <div className="custom-control custom-radio custom-control-inline">
-                            <input 
-                              type="radio" 
-                              id={`customRadioInline${pregunta.tracker}-${index}`} 
-                              name={`customRadioInline${pregunta.tracker}`} 
-                              className="custom-control-input" 
-                              value={opcion}
-                            />
-                            <label 
-                              className="custom-control-label" 
-                              htmlFor={`customRadioInline${pregunta.tracker}-${index}`}>
-                                {opcion}
-                            </label>
-                          </div>
-                        </Col>
+        {formulario ? 
+          <div>
+            <Form onSubmit={enviarDatos}>
+              <div style={style}>
+                <Row>
+                  <Col>
+                    <hr/>
+                      <h1 className="text-left">1) DATOS DE USUARIO:</h1>
+                    <hr/>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col style={{marginTop: '15px'}}>
+                    {datosPersonalesPreguntas.map((pregunta, index) => (
+                      <Form.Control 
+                        key={index} 
+                        name={pregunta.valor} 
+                        placeholder={pregunta.nombre} 
+                        type={pregunta.tipo} 
+                        style={{marginBottom: '10px'}}
+                        
+                      />
                     ))}
-                  </Row>
-                  <hr/>
-                </div>
-              ))}
-            </Col>
-          </Row>
-          <Button type='submit' variant="outline-light" className="cambioSeccion">ENVIAR ENCUESTA</Button>
-        </div>
-      </Form>
+                  </Col>
+                </Row>
+                <Button variant="outline-light" className="cambioSeccion" onClick={datosPersonales}>SIGUIENTE</Button>
+              </div>
+              <div style={styleDos}>
+                <Row>
+                  <Col>
+                    <hr/>
+                      <h1 className="text-left font-weight-bold">2) ATENCIÓN AL CLIENTE:</h1>
+                    <hr/>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col style={{marginTop: '15px'}}>
+                    {datosImcycPreguntas.map((pregunta, i) => (
+                      <div key={i} style={{marginBottom: '40px'}}>
+                        <h3 className="text-left" className="numero">{i + 1}</h3>
+                        <h2 className="text-left preguntame">{pregunta.pregunta}</h2>
+                        <Row>
+                          {pregunta.opciones.length > 0 ? 
+                            pregunta.opciones.map((opcion, index) => (
+                              <Col key={index}>
+                                <div className="custom-control custom-radio custom-control-inline">
+                                  <input 
+                                    type="radio" 
+                                    id={`customRadioInline${pregunta.tracker}-${index}`} 
+                                    name={`customRadioInline${pregunta.tracker}`} 
+                                    className="custom-control-input" 
+                                    value={opcion}
+                                    required
+                                  />
+                                  <label 
+                                    className="custom-control-label" 
+                                    htmlFor={`customRadioInline${pregunta.tracker}-${index}`}>
+                                      {opcion}
+                                  </label>
+                                </div>
+                              </Col>
+                            ))
+                            :
+                            <Form.Group controlId="exampleForm.ControlTextarea1" style={{width: '100%'}}>
+                              <Form.Control as="textarea" rows="3" style={{width: '100%'}} />
+                            </Form.Group>
+                          }
+                        </Row>
+                        <hr/>
+                      </div>
+                    ))}
+                  </Col>
+                </Row>
+                <Button type='submit' variant="outline-light" className="cambioSeccion">
+                  ENVIAR ENCUESTA
+                </Button>
+              </div>
+            </Form>
+          </div> 
+          : 
+          <div>
+            <h1><b>MUCHAS GRACIAS!!!</b></h1>
+            <h2><b>Su datos han sido enviados satisfactoriamente!!!</b></h2>
+          </div>
+        }
       </Container>
     </div>
   )
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from '../images/logo-imcyc.svg';
 
 const Header = () => (
   <div>
-    <Logo style={{width: '200px',marginBottom: '20px',marginTop: '20px'}} />
+    <Link to="/" style={{background: 'none'}}>
+      <Logo style={{width: '200px',marginBottom: '20px',marginTop: '20px'}} />
+    </Link>
     <h1>INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</h1>
     <h2>FORMATO DE EVALUACIÓN DE SERVICIOS IMCYC</h2>
   </div>
